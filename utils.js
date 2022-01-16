17e12f55a5339de608c15cc99c0d910f3c3b03a6
James Stevenson <mooseling@hotmail.com>
2022-01-16 15:34:54 +0000

    Convert utils to js
    A lot of this is stuff to run in browser devtools

--- utils.js
+++ utils.js
@@ -1,17 +1,17 @@
+/* jshint esversion:6 */
+
 // We want to sort the dictionary so that the best guesses are at the top
 // Currently this is just based on a letter-variety score
 // Words like "doddy" that guess a single letter 3 times are really bad
-
-export function sort(dictionary:string[]): string[] {
-  const gradedDictionary: {word:string, grade:number}[] = dictionary.map(word => ({word, grade:grade(word)}));
+export function sort(dictionary) {
+  const gradedDictionary = dictionary.map(word => ({word, grade:grade(word)}));
   const sortedGradedDictionary = gradedDictionary.sort((a, b) => b.grade - a.grade); // Sort desc
   return sortedGradedDictionary.map(({word}) => word);
 }
 
 
-
-function grade(word:string): number {
-  const lettersInWord:string[] = [];
+function grade(word) {
+  const lettersInWord = [];
   [...word].forEach(letter => {
     if (!lettersInWord.includes(letter))
       lettersInWord.push(letter);
